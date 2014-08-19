a83932c4fa7aecf13a09a655b538243e342169fc
daowzq <wu922008@126.com>
2014-08-19 20:54:54 +0800

    add

---
+++ Razor/Common/DynamicJson/Bson/BsonDocument.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Razor.DynamicJson.Bson
+{
+  internal class BsonDocument
+  {
+    /// <summary>
+    /// 父文档
+    /// </summary>
+    public BsonDocument Parent;
+    /// <summary>
+    /// 文档长度
+    /// </summary>
+    public int Length;
+    /// <summary>
+    /// 文档摘要Byte的数量
+    /// </summary>
+    public int Digested;
+  }
+}
