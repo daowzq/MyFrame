ecae7e65069a89c446a1714c678761db5b9683a0
daowzq <wu922008@126.com>
2014-08-22 22:51:12 +0800

    add new file

---
+++ Razor/Common/DataTableToPdf.cs
@@ -0,0 +1,100 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Razor
+{
+    public class DataTableToPdf
+    {
+        /// <summary>
+        /// DataTable导出到PDF
+        /// </summary>
+        /// <param name="datatable">DataTable</param>
+        /// <param name="PDFFilePath">导出的PDF存储路径</param>
+        /// <param name="PdfSaveTitle">导出的文件名</param>
+        /// <param name="FontPath">字体路径</param>
+        /// <param name="FontSize">字体大小</param>
+        /// <param name="widthmz">每一列宽度</param>
+        /// <returns>布尔值</returns>
+        public static string ConvertDataTableToPDF(DataTable datatable, string PDFFilePath, string PdfSaveTitle, string FontPath, float FontSize, float[] widthmz)
+        {
+            string strReturnSaveFileName = "";//用于返回的文件名
+            if (widthmz == null || widthmz.Length == 0)//如果每一列宽度未指定
+            {
+                widthmz = new float[datatable.Columns.Count];
+                for (int i = 0; i < datatable.Columns.Count; i++)
+                {
+                    widthmz[i] = 11f;
+                }
+            }
+            //初始化一个目标文档类
+            Document document = new Document();
+            //调用PDF的写入方法流
+
+            //注意FileMode-Create表示如果目标文件不存在，则创建，如果已存在，则覆盖。
+            PdfWriter writer = PdfWriter.getInstance(document, new FileStream(PDFFilePath + "\\" + PdfSaveTitle + ".pdf", FileMode.Create));
+            strReturnSaveFileName = PDFFilePath + "\\" + PdfSaveTitle + ".pdf";
+            try
+            {
+                //打开目标文档对象
+                document.Open();
+
+                // 添加页眉
+                HeaderFooter header = new HeaderFooter(new Phrase(PdfSaveTitle), false);
+                document.Header = header;
+                // 添加页脚
+                HeaderFooter footer = new HeaderFooter(new Phrase(PdfSaveTitle), true);
+                footer.Border = Rectangle.NO_BORDER;
+                document.Footer = footer;
+
+                //创建PDF文档中的字体
+                BaseFont baseFont = BaseFont.createFont(FontPath, BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+                //根据字体路径和字体大小属性创建字体
+                Font font = new Font(baseFont, FontSize);
+
+                Paragraph pTitle = new Paragraph(new Chunk(PdfSaveTitle, FontFactory.getFont(FontFactory.HELVETICA, 12)));
+                document.Add(pTitle);
+
+                //根据数据表内容创建一个PDF格式的表
+                PdfPTable table = null;
+                table = new PdfPTable(widthmz);
+
+
+                //打印列名
+                for (int j = 0; j < datatable.Columns.Count; j++)
+                {
+                    table.addCell(new Phrase(datatable.Columns[j].ColumnName.ToString(), font));
+                }
+
+
+                //遍历原table的内容
+                for (int i = 0; i < datatable.Rows.Count; i++)
+                {
+                    for (int j = 0; j < datatable.Columns.Count; j++)
+                    {
+                        table.addCell(new Phrase(datatable.Rows[i][j].ToString(), font));
+                    }
+                }
+                //在目标文档中添加转化后的表数据
+                document.Add(table);
+            }
+            catch (Exception ec)
+            {
+                strReturnSaveFileName = "";
+            }
+            finally
+            {
+                //关闭目标文件
+                document.Close();
+                //关闭写入流
+                writer.Close();
+            }
+            return strReturnSaveFileName;
+        }
+    }
+}
--- Razor/Common/SysInfoHelper.cs
+++ Razor/Common/SysInfoHelper.cs
@@ -1,12 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 
 namespace Razor
 {
     public sealed class SysInfoHelper
     {
-        
+        /// <summary>
+        /// 获取本机IP地址列表
+        /// </summary>
+        /// <returns></returns>
+        public static string[] GetLocalHostIP()
+        {
+            IPHostEntry hostinfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress[] ips = hostinfo.AddressList;
+            string[] strLocalHostIPs = new string[ips.Length];
+
+            for (int index = 0; index < ips.Length; index++)
+            {
+                strLocalHostIPs[index] = ips[index].ToString();
+            }
+            return strLocalHostIPs;
+        }
+
+        /// <summary>
+        /// 获取本机主机名
+        /// </summary>
+        /// <returns></returns>
+        public static string GetLocalHostName()
+        {
+            string strHostName = Dns.GetHostName();
+            return strHostName;
+        }
+
+        /// <summary>
+        /// 获取新的Process 组件并将其与当前活动的进程关联的主模块的完整路径，包含文件名(进程名)
+        /// </summary>
+        public static string CurrentProcessModuleName()
+        {
+            return System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName;
+        }
+
+        /// <summary>
+        /// 获取和设置当前目录（即该进程从中启动的目录）的完全限定路径
+        /// </summary>
+        public static string CurrentDirectory()
+        {
+            return System.Environment.CurrentDirectory; ;
+        }
+
+        /// <summary>
+        ///当前Thread 的当前应用程序域的基目录 
+        /// </summary>
+        public static string CurrentDomainDic()
+        {
+            return System.AppDomain.CurrentDomain.BaseDirectory;
+        }
+
+        /// <summary>
+        ///获取应用程序的当前工作目录 
+        /// </summary>
+        public static string GetCurrentDirectory()
+        {
+            return System.IO.Directory.GetCurrentDirectory();
+        }
     }
 }
