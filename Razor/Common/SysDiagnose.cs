1c6f6767140afb2536ccccc3269f6ef7d1552826
daowzq <wu922008@126.com>
2014-08-24 11:40:07 +0800

    add new code

---
+++ Razor/Common/SysDiagnose.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Razor
+{
+    public sealed class SysDiagnotiseHelper
+    {
+        /// <summary>
+        /// 检测性能类,包括GC的释放次数,
+        /// 该实例对象被释放的时候会Console输出测试结果
+        /// </summary>
+        public sealed class OperationTimer : IDisposable
+        {
+            private Int64 m_startTime;
+            private String m_text;
+            private int m_collectioncount;
+            public OperationTimer(string text)
+            {
+                PrepareForOpenation();
+                m_text = text;
+                m_collectioncount = GC.CollectionCount(0);
+                m_startTime = Stopwatch.GetTimestamp(); //该类实例化时间
+            }
+            public void Dispose()
+            {
+                Console.WriteLine("{0,6:###.00} seconds (Gcs={1,3}) {2}", (Stopwatch.GetTimestamp() - m_startTime) /
+                    (Double)Stopwatch.Frequency, GC.CollectionCount(0) - m_collectioncount, m_text);
+            }
+            private static void PrepareForOpenation()
+            {
+                GC.Collect();
+                GC.WaitForPendingFinalizers();//挂起当前线程,直到处理终结器队列的线程清空该队列为止
+                GC.Collect();
+                //这里进行了两次Collect 调用
+            }
+        }
+    }
+}
--- Razor/Razor/Program.cs
+++ Razor/Razor/Program.cs
@@ -10,93 +10,112 @@ namespace Razor
 {
     class Program
     {
-        //    static void Main(string[] args)
-        //    {
-        //        ////string str1 = "2014-8-29";
-        //        //List<string> lt = new List<string>();
-        //        //lt.Add("hello");
-        //        //lt.Add("world");
-        //        //string result = StringHelper.Join(lt);
-        //        //Console.WriteLine(result);
-
-        //        //MailHelper.SendMailHelper.mailAccount = "你妹啊";
-        //        //MailHelper.SendMailHelper.mailPwd = "wu19920101110";
-        //        //MailHelper.SendMailHelper.smtpHost = "smtp.126.com";
-        //        //MailHelper.SendMailHelper.mailSenderAddress = "wu922008@126.com";
-        //        //string cc = "2803974130@qq.com";
-        //        //string title = "测试哦";
-        //        //string body = "这是测试内容哦!";
-        //        //List<string> list = new List<string>();
-        //        //list.Add(cc);
-        //        //MailHelper.SendMailHelper.SendMailAndCC("568909447@qq.com", title, body, list);
-
-        //        //  string result = SysInfoHelper.CurrentProcessModuleName();
-        //        //  Console.WriteLine(result);
-
-        //        //string str1 = "hello world";
-        //        //string result = SecurityHelper.MD5Encrypt(str1);
-        //        //Console.WriteLine(result);
-
-        //        //Console.WriteLine(SecurityHelper.Des3DecryptStr(result));
-
-        //        // string outPath = @"D:\test1.txt";
-        //        // SecurityHelper.EncryptFile(@"D:\test.txt", outPath);
-        //        // SecurityHelper.DencryptFile(outPath, @"d:\resolver.txt");
-
-        //        string privateKey, publicKey;
-        //        SecurityHelper.CreateRSAPairKey(out privateKey, out publicKey);
-        //        // Console.WriteLine(privateKey);
-        //        string result;
-        //        //result = SecurityHelper.RSAEcryptString("hello world", publicKey);
-        //        //Console.WriteLine(result);
-
-        //        result = SecurityHelper.RSAEncryptString("你好,世界", 1024, publicKey);
-        //        Console.WriteLine(result);
-
-        //        result = SecurityHelper.RSADecryptString(result, 1024, privateKey);
-        //        Console.WriteLine(result);
-        static void Main()
+        static void Main(string[] args)
         {
-            try
-            {
-                //Create a UnicodeEncoder to convert between byte array and string.
-                UnicodeEncoding ByteConverter = new UnicodeEncoding();
+            //        ////string str1 = "2014-8-29";
+            //        //List<string> lt = new List<string>();
+            //        //lt.Add("hello");
+            //        //lt.Add("world");
+            //        //string result = StringHelper.Join(lt);
+            //        //Console.WriteLine(result);
 
-                //Create byte arrays to hold original, encrypted, and decrypted data. 
-                byte[] dataToEncrypt = ByteConverter.GetBytes("Data to Encrypt");
-                byte[] encryptedData;
-                byte[] decryptedData;
+            //        //MailHelper.SendMailHelper.mailAccount = "你妹啊";
+            //        //MailHelper.SendMailHelper.mailPwd = "wu19920101110";
+            //        //MailHelper.SendMailHelper.smtpHost = "smtp.126.com";
+            //        //MailHelper.SendMailHelper.mailSenderAddress = "wu922008@126.com";
+            //        //string cc = "2803974130@qq.com";
+            //        //string title = "测试哦";
+            //        //string body = "这是测试内容哦!";
+            //        //List<string> list = new List<string>();
+            //        //list.Add(cc);
+            //        //MailHelper.SendMailHelper.SendMailAndCC("568909447@qq.com", title, body, list);
 
-                //Create a new instance of RSACryptoServiceProvider to generate 
-                //public and private key data. 
+            //        //  string result = SysInfoHelper.CurrentProcessModuleName();
+            //        //  Console.WriteLine(result);
 
-                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
-                {
-                    string privateKey = RSA.ToXmlString(true);
-                    string publicKey = RSA.ToXmlString(true);
-                    encryptedData = RSAEncrypt(dataToEncrypt, privateKey, false);
+            //        //string str1 = "hello world";
+            //        //string result = SecurityHelper.MD5Encrypt(str1);
+            //        //Console.WriteLine(result);
 
-                    decryptedData = RSADecrypt(encryptedData, publicKey, false);
+            //        //Console.WriteLine(SecurityHelper.Des3DecryptStr(result));
 
-                    //Display the decrypted plaintext to the console. 
-                    Console.WriteLine("Decrypted plaintext: {0}", ByteConverter.GetString(decryptedData));
-                }
-            }
-            catch (ArgumentNullException)
-            {
-                //Catch this exception in case the encryption did 
-                //not succeed.
-                Console.WriteLine("Encryption failed.");
+            //        // string outPath = @"D:\test1.txt";
+            //        // SecurityHelper.EncryptFile(@"D:\test.txt", outPath);
+            //        // SecurityHelper.DencryptFile(outPath, @"d:\resolver.txt");
 
-            }
-            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
-            string prKey = rsa.ToXmlString(true);
-            string puKey = rsa.ToXmlString(true);
+            //        string privateKey, publicKey;
+            //        SecurityHelper.CreateRSAPairKey(out privateKey, out publicKey);
+            //        // Console.WriteLine(privateKey);
+            //        string result;
+            //        //result = SecurityHelper.RSAEcryptString("hello world", publicKey);
+            //        //Console.WriteLine(result);
 
-            string result = SecurityHelper.RSAEcryptString("hello world", puKey);
-            SecurityHelper.RSADcryptString(result, prKey);
+            //        result = SecurityHelper.RSAEncryptString("你好,世界", 1024, publicKey);
+            //        Console.WriteLine(result);
+
+            //        result = SecurityHelper.RSADecryptString(result, 1024, privateKey);
+            //        Console.WriteLine(result);
+
+
+
+
+
+            //string result = Razor.Serializer.SerializeToBase64(st);
+            //Console.WriteLine(result);
+            //st = (Student)Razor.Serializer.DeserializeFromBase64(result);
+
+            Student st = new Student();
+            st.Age = 12;
+            st.Class = "2.12";
+            st.Name = "小明";
+            st.Teacher = "张明";
+
 
         }
+
+        #region
+        //static void Main()
+        //{
+        //    try
+        //    {
+        //        //Create a UnicodeEncoder to convert between byte array and string.
+        //        UnicodeEncoding ByteConverter = new UnicodeEncoding();
+
+        //        //Create byte arrays to hold original, encrypted, and decrypted data. 
+        //        byte[] dataToEncrypt = ByteConverter.GetBytes("Data to Encrypt");
+        //        byte[] encryptedData;
+        //        byte[] decryptedData;
+
+        //        //Create a new instance of RSACryptoServiceProvider to generate 
+        //        //public and private key data. 
+
+        //        using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+        //        {
+        //            string privateKey = RSA.ToXmlString(true);
+        //            string publicKey = RSA.ToXmlString(true);
+        //            encryptedData = RSAEncrypt(dataToEncrypt, privateKey, false);
+
+        //            decryptedData = RSADecrypt(encryptedData, publicKey, false);
+
+        //            //Display the decrypted plaintext to the console. 
+        //            Console.WriteLine("Decrypted plaintext: {0}", ByteConverter.GetString(decryptedData));
+        //        }
+        //    }
+        //    catch (ArgumentNullException)
+        //    {
+        //        //Catch this exception in case the encryption did 
+        //        //not succeed.
+        //        Console.WriteLine("Encryption failed.");
+
+        //    }
+        //    RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+        //    string prKey = rsa.ToXmlString(true);
+        //    string puKey = rsa.ToXmlString(true);
+
+        //    string result = SecurityHelper.RSAEcryptString("hello world", puKey);
+        //    SecurityHelper.RSADcryptString(result, prKey);
+
+        //}
 
         static public byte[] RSAEncrypt(byte[] DataToEncrypt, string RSAKeyInfo, bool DoOAEPPadding)
         {
@@ -158,6 +177,15 @@ namespace Razor
             }
 
         }
+        #endregion
+    }
 
+    [Serializable]
+    class Student
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string Teacher { get; set; }
     }
 }
