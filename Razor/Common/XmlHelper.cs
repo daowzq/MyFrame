8bdeebc2d270d02f1a188d35cbc374a3ae74e890
daowzq <wu922008@126.com>
2014-09-17 22:03:27 +0800

    add new code

--- Razor/Common/XmlHelper.cs
+++ Razor/Common/XmlHelper.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Web.Script.Serialization;
 using System.Xml;
 
 namespace Razor
@@ -18,6 +20,72 @@ namespace Razor
             //
         }
 
+        //public static XmlDocument Json2XmlStrict(string json)
+        //{
+        //    XmlDictionaryReader reader = JsonReaderWriterFactory.CreateJsonReader(Encoding.UTF8.GetBytes(json), XmlDictionaryReaderQuotas.Max);
+        //    XmlDocument doc = new XmlDocument();
+        //    doc.Load(reader);
+        //    return doc;
+        //}
+
+        /// <summary>
+        /// json字符串转换为Xml对象
+        /// </summary>
+        /// <param name="sJson"></param>
+        /// <returns></returns>
+        public static XmlDocument Json2Xml(string sJson)
+        {
+            //XmlDictionaryReader reader = JsonReaderWriterFactory.CreateJsonReader(Encoding.UTF8.GetBytes(sJson), XmlDictionaryReaderQuotas.Max);
+            //XmlDocument doc = new XmlDocument();
+            //doc.Load(reader);
+
+            JavaScriptSerializer oSerializer = new JavaScriptSerializer();
+            Dictionary<string, object> Dic = (Dictionary<string, object>)oSerializer.DeserializeObject(sJson);
+            XmlDocument doc = new XmlDocument();
+            XmlDeclaration xmlDec;
+            xmlDec = doc.CreateXmlDeclaration("1.0", "utf-8", "yes");
+            doc.InsertBefore(xmlDec, doc.DocumentElement);
+            XmlElement nRoot = doc.CreateElement("root");
+            doc.AppendChild(nRoot);
+            foreach (KeyValuePair<string, object> item in Dic)
+            {
+                XmlElement element = doc.CreateElement(item.Key);
+                KeyValue2Xml(element, item);
+                nRoot.AppendChild(element);
+            }
+            return doc;
+        }
+
+        private static void KeyValue2Xml(XmlElement node, KeyValuePair<string, object> Source)
+        {
+            object kValue = Source.Value;
+            if (kValue.GetType() == typeof(Dictionary<string, object>))
+            {
+                foreach (KeyValuePair<string, object> item in kValue as Dictionary<string, object>)
+                {
+                    XmlElement element = node.OwnerDocument.CreateElement(item.Key);
+                    KeyValue2Xml(element, item);
+                    node.AppendChild(element);
+                }
+            }
+            else if (kValue.GetType() == typeof(object[]))
+            {
+                object[] o = kValue as object[];
+                for (int i = 0; i < o.Length; i++)
+                {
+                    XmlElement xitem = node.OwnerDocument.CreateElement("Item");
+                    KeyValuePair<string, object> item = new KeyValuePair<string, object>("Item", o[i]);
+                    KeyValue2Xml(xitem, item);
+                    node.AppendChild(xitem);
+                }
+
+            }
+            else
+            {
+                XmlText text = node.OwnerDocument.CreateTextNode(kValue.ToString());
+                node.AppendChild(text);
+            }
+        }
 
         #region XML文档节点查询和读取
         /// <summary>
@@ -113,7 +181,7 @@ namespace Razor
             try
             {
                 XmlDocument xmlDoc = new XmlDocument();
-                XmlDeclaration xmlDeclaration = xmlDoc.CreateXmlDeclaration(version, encoding,standalone);
+                XmlDeclaration xmlDeclaration = xmlDoc.CreateXmlDeclaration(version, encoding, standalone);
                 XmlNode root = xmlDoc.CreateElement(rootNodeName);
                 xmlDoc.AppendChild(xmlDeclaration);
                 xmlDoc.AppendChild(root);
